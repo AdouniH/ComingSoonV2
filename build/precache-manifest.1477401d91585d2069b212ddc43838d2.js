6240eca6a6feda386d1f8d78aa8129f52c2ac2d2
AdouniH <houssemadouni11@gmail.com>
2019-12-21 20:30:09 +0100

    save changes

---
+++ build/precache-manifest.1477401d91585d2069b212ddc43838d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0382b405890c29b27d1430f90494d161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36653cececfb3d84be26",
+    "url": "/static/css/main.92da9e9d.chunk.css"
+  },
+  {
+    "revision": "a42b6880101f67c10c2c",
+    "url": "/static/js/2.9bbd466e.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.9bbd466e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36653cececfb3d84be26",
+    "url": "/static/js/main.f7b212f8.chunk.js"
+  },
+  {
+    "revision": "a9f50561b24de2fdb52e",
+    "url": "/static/js/runtime-main.c4e7cc5b.js"
+  },
+  {
+    "revision": "f5a8a153a6907cd7f356ee8f9fd141ab",
+    "url": "/static/media/back.f5a8a153.jpg"
+  },
+  {
+    "revision": "0d066e2e2c1280750896ba74d736455c",
+    "url": "/static/media/background.0d066e2e.jpg"
+  }
+]);
